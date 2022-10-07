e12548f8c642a31b2108f98c20a59eb04c73329d
QwertyBobberson <cebrhim@gmail.com>
2022-10-07 00:24:21 -0400

    Animated noise on gpu

---
+++ main.cpp
@@ -0,0 +1,147 @@
+#include "Application.cpp"
+#include <typeinfo>
+using namespace std;
+
+
+void DrawSquare(double x, double y, double z, double width, double height, double r, double g, double b)
+{
+	int start = verts.size();
+	verts.push_back({{x			, y			, z}, {r, g, b}});
+	verts.push_back({{x + width	, y			, z}, {r, g, b}});
+	verts.push_back({{x			, y + height, z}, {r, g, b}});
+	verts.push_back({{x + width	, y + height, z}, {r, g, b}});
+
+	indices.push_back(start + 0);
+	indices.push_back(start + 1);
+	indices.push_back(start + 2);
+	indices.push_back(start + 3);
+	indices.push_back(start + 2);
+	indices.push_back(start + 1);
+}
+
+void DrawSquareFacingCamera(double x, double y, double width, double height, double r, double g, double b)
+{
+	if(width < 0)
+	{
+		x += width;
+		width *= -1;
+	}
+	if(height < 0)
+	{
+		y += height;
+		height *= -1;
+	}
+
+	DrawSquare(x, y, 0, width, height, r, g, b);
+}
+
+int main(int argc, char** argv)
+{
+	Application app;
+
+	// DrawSquare(-0.5, -0.5, 1, 1, 0, 0, 1);
+
+	// PNG png = ReadPng("test2.png");
+
+	// std::cout << png.height << " " << png.width << std::endl;
+
+	int sHeight = HEIGHT;
+	int sWidth = WIDTH;
+
+	// DrawSquare(-0.5, -0.5, 0, 1, 1, 0, 0, 0);
+	DrawSquareFacingCamera(-1, -1, 2, 2, 0, 0, 1);
+
+	// for(int y = -sHeight; y < sHeight; y++)
+	// {
+	// 	for(int x = -sWidth; x < sWidth; x++)
+	// 	{
+	// 		int xIndex = x == -sWidth ? 0 : floor((double)png.width/((x + sWidth)/2.0)) * 4;
+	// 		int yIndex = y == -sHeight ? 0 : floor((double)png.height/((y + sHeight)/2.0));
+	// 		std::cout << x << " " << y << std::endl;
+	// 		std::cout << xIndex << " " << yIndex << std::endl;
+	// 		double r = (png.pRows[yIndex][xIndex + 0])/255.0;
+	// 		double g = (png.pRows[yIndex][xIndex + 1])/255.0;
+	// 		double b = (png.pRows[yIndex][xIndex + 2])/255.0;
+
+	// 		// double xPos = (double)x/png.width;
+	// 		// std::cout << y << " " << xPos << " " << x << std::endl;
+
+	// 		// double yPos = (double)y/png.height;
+	// 		// std::cout << y << " " << yPos << " " << x << std::endl;
+
+	// 		// double pWidth = 1.0/png.width;
+	// 		// std::cout << y << " " << pWidth << " " << x << std::endl;
+
+	// 		// double pHeight = 1.0/png.height;
+	// 		// std::cout << y << " " << pHeight << " " << x << std::endl;
+
+	// 		// if(r != 0 || g != 0 || b != 0)
+	// 		// {
+	// 		// 	std::cout << y << " " << r << " " << x << std::endl;
+	// 		// 	std::cout << y << " " << g << " " << x << std::endl;
+	// 		// 	std::cout << y << " " << b << " " << x << std::endl;
+	// 		// }
+
+	// 		// std::cout << ((double)x/WIDTH) << std::endl;
+	// 		DrawSquare(((double)x/WIDTH), ((double)y/HEIGHT), 1.0/WIDTH, 1.0/HEIGHT, r, g, b);
+	// 	}
+	// }
+
+	// std::cout << (int)png.colorType << std::endl;
+	//   << png.pRows[png.height - 1][png.rows - 1][3] << ", ";
+
+	// verts.resize(10);
+	// indices.resize(10);
+	
+	// std::cout << "Validation Layers: " << enableValidationLayers<< std::endl;
+	
+	// PNG png = ReadPng("test.png");
+	
+	
+	// verts = 
+	// {
+	// 	{{-0.5f, -0.5f}, {1.0f, 0.0f, 0.0f}, {1.0f, 0.0f}},
+	// 	{{ 0.5f, -0.5f}, {0.0f, 1.0f, 0.0f}, {0.0f, 0.0f}},
+	// 	{{ 0.5f,  0.5f}, {0.0f, 0.0f, 1.0f}, {0.0f, 1.0f}},
+	// 	{{-0.5f,  0.5f}, {0.0f, 0.0f, 0.0f}, {1.0f, 1.0f}}
+	// };
+
+	// indices = 
+	// {
+	// 	0, 1, 2, 
+	// 	2, 3, 0
+	// };
+
+	// DrawSquare(-0.5, -0.5, -0.5, 1, 1, 0, 0, 1);
+	// // DrawSquare(-0.5, -0.5, 0, 1, 1, 0, 0, 1);
+	// DrawSquare(-0.5, -0.5, 0.5, 1, 1, 0, 0, 1);
+	// DrawSquare(-0.5, -0.5, 1, 1, 1, 0, 0, 1);
+
+	// double size = std::stod(argv[1]);
+	// for(int y = -size; y < size; y += 1)
+	// {
+	// 	for(int x = -size; x < size; x += 1)
+	// 	{
+	// 		if((x + y) % 2 == 0)
+	// 		{
+	// 			DrawSquare(x/size, y/size, 1/size, 1/size, 0, 0.05, 0.95);
+	// 		}
+	// 		else
+	// 		{
+	// 			DrawSquare(x/size, y/size, 1/size, 1/size, 0.005, 0.005, 0.005);
+	// 		}
+	// 	}
+	// }
+	try
+	{
+		app.Run(verts, indices);
+	}
+	catch(const std::exception& e)
+	{
+		std::cerr << e.what() << '\n';
+		return EXIT_FAILURE;
+	}
+
+	return EXIT_SUCCESS;
+	
+}
---
+++ shaders/shader.frag
@@ -0,0 +1,96 @@
+#version 450
+#extension GL_EXT_debug_printf : enable
+
+layout(location = 0) in vec3 fragColor;
+layout(location = 0) out vec4 outColor;
+
+layout(binding = 0) uniform UniformBufferObject
+{
+    float z;
+} ubo;
+
+
+int octaves = 25;
+float scale = 500;
+float lacunarity = 2;
+float persistence = .5f;
+float offsetNoiseWeight = 10000;
+float offsetNoiseScale = 1000;
+float xOffset = 100;
+float yOffset = 100;
+
+vec3 GenerateVector(int x, int y, int z)
+{
+    float j = 4096.0*sin(dot(vec3(x, y, z),vec3(17.0, 59.4, 15.0)));
+	vec3 r;
+	r.z = fract(512.0*j);
+	j *= .125;
+	r.x = fract(512.0*j);
+	j *= .125;
+	r.y = fract(512.0*j);
+    r -= 0.5;
+    r = normalize(r);
+	return r;
+}
+
+float SmoothStep(float x, float min, float max)
+{
+    return ((x * x * x * (x * (x * 6 - 15) + 10)) * (max-min)) + min;
+}
+
+
+float GetPoint(float x, float y, float z)
+{
+    float dotProducts[2][2][2];
+    vec3 distance = vec3(x - floor(x), y - floor(y), z - floor(z));
+    vec3 offset;
+    vec3 gradient;
+    float dotProduct;
+
+    for(int _z = 0; _z < 2; _z++)
+    {
+        for(int _y = 0; _y < 2; _y++)
+        {
+            for(int _x = 0; _x < 2; _x++)
+            {
+                offset.x = _x - distance.x;
+                offset.y = _y - distance.y;
+                offset.z = _z - distance.z;
+
+                gradient = GenerateVector(int(x + offset.x), int(y + offset.y), int(z + offset.z));
+                
+                dotProducts[_z][_y][_x] = dot(offset, gradient);
+            }
+        }
+    }
+
+
+
+    float xy1z1 = SmoothStep(distance.z, dotProducts[0][0][0], dotProducts[1][0][0]);
+    float xy2z1 = SmoothStep(distance.z, dotProducts[0][1][0], dotProducts[1][1][0]);
+    float xy1z2 = SmoothStep(distance.z, dotProducts[0][0][1], dotProducts[1][0][1]);
+    float xy2z2 = SmoothStep(distance.z, dotProducts[0][1][1], dotProducts[1][1][1]);
+    float xyz1 = SmoothStep(distance.y, xy1z1, xy2z1);
+    float xyz2 = SmoothStep(distance.y, xy1z2, xy2z2);
+    float xyz = SmoothStep(distance.x, xyz1, xyz2);
+
+
+    return xyz;
+
+}
+void main()
+{
+    float amplitude = 2;
+    float frequency = 1;
+    float noiseValue = 0;
+    float zFreq = .25;
+
+    float offsetNoise = offsetNoiseWeight * GetPoint(gl_FragCoord.x/offsetNoiseScale, gl_FragCoord.y/offsetNoiseScale, ubo.z * zFreq);
+    for(int i = 0; i < octaves; i++)
+    {
+        noiseValue += GetPoint((gl_FragCoord.x + xOffset + offsetNoise)/scale * frequency, (gl_FragCoord.y + yOffset + offsetNoise)/scale * frequency, ubo.z * zFreq) * amplitude;
+        amplitude *= persistence;
+        frequency *= lacunarity;
+    }
+    outColor = vec4(0, 0, noiseValue * noiseValue * noiseValue, 1.0);
+}
